5feb894821eb30305cf22dcd58e4ec06a3204dc4
Erick Martins <ericknmp@gmail.com>
2022-06-27 19:04:47 -0300

    feat: login and signup screens

--- lint-staged.js
+++ lint-staged.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'*.{js,jsx,ts,tsx}': [
-		'yarn eslint "**/*.{js,jsx,ts,tsx}" --max-warnings=0 --fix',
+		'yarn eslint --max-warnings=0 --fix',
 		'yarn react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
 		() => 'yarn tsc-files --noEmit'
 	],
